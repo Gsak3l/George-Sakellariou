d60cab93d4a43bf8a07a9c8064b8f74f8ba89ac4
gsak3l <gsakel5@gmail.com>
2021-01-28 21:57:07 +0200

    added and deletd some images, made some images appear with ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'george-sakellariou';
 }
+
+export let all_projects = [
+  {
+    category: 'WEB DESIGN',
+    title: 'FINEAS RACING TEAM',
+    header: 'DESIGN AND DEVELOPMENT',
+    details:
+      'I designed with the help of George Stinis the website for the FRT (Fineas Racing Team), where many ambitious people' +
+      'decided to try building a driverless vehicle. Unfortunately the Project was shut down after two years of hard work.',
+    details_link: 'https://fineasracingteam.netlify.app/',
+  },
+  {
+    category: 'WEB DESIGN',
+    title: 'FINEAS RACING TEAM',
+    header: 'DESIGN AND DEVELOPMENT',
+    details:
+      'I designed with the help of George Stinis the website for the FRT (Fineas Racing Team), where many ambitious people' +
+      'decided to try building a driverless vehicle. Unfortunately the Project was shut down after two years of hard work.',
+    details_link: 'https://fineasracingteam.netlify.app/',
+  },
+  {
+    category: 'WEB DESIGN',
+    title: 'FINEAS RACING TEAM',
+    header: 'DESIGN AND DEVELOPMENT',
+    details:
+      'I designed with the help of George Stinis the website for the FRT (Fineas Racing Team), where many ambitious people' +
+      'decided to try building a driverless vehicle. Unfortunately the Project was shut down after two years of hard work.',
+    details_link: 'https://fineasracingteam.netlify.app/',
+  },
+];
--- src/app/components/featured/featured.component.ts
+++ src/app/components/featured/featured.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { all_projects } from '../../app.component';
 
 @Component({
   selector: 'app-featured',
   templateUrl: './featured.component.html',
-  styleUrls: ['./featured.component.sass']
+  styleUrls: ['./featured.component.sass'],
 })
 export class FeaturedComponent implements OnInit {
+  projects = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    for (var i = 0; i < all_projects.length; i++) {
+      this.projects.push(all_projects[i]);
+    }
   }
-
 }
